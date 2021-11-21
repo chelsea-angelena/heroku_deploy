ea43abc5526b342a4fd54b5ef8934603909cfe35
Chelsea Angelena <chelsea.angelena@gmail.com>
2021-11-21 08:35:16 -0800

    changed port

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 5002;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  console.log(`listening on port: ${await app.getUrl()}`);
 }
 bootstrap();
