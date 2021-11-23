7b0762f85d7b0fffb6c913441731914e90a82263
Chelsea Angelena <chelsea.angelena@gmail.com>
2021-11-22 22:00:06 -0800

    removed entity columns

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,25 +1,18 @@
 import { Column, Entity } from 'typeorm';
-import { Exclude } from 'class-transformer';
 import { BaseEntity } from '../../common/entities/base.entity';
 
 @Entity()
 export class User extends BaseEntity {
   @Column({ nullable: true })
-  iss: string;
-
-  @Column({ nullable: true })
   sub: string;
 
   @Column({ nullable: true })
-<<<<<<< HEAD
   iat: number;
 
   @Column({ nullable: true })
   exp: number;
 
   @Column({ nullable: true })
-=======
->>>>>>> 6bd5b5c (added dependenccies)
   azp: string;
 
   @Column({ nullable: true })
